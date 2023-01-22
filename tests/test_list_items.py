e53e14d067ac5ae3419b9bd4a285bc9740386811
The Sam <muer.powell@gmail.com>
2023-01-22 17:16:14 +1300

    Organizing, and length tests

--- tests/test_list_items.py
+++ tests/test_list_items.py
@@ -28,7 +28,44 @@ class ListItemsTests(unittest.TestCase):
         self.assertEqual(str(my_list), expected)
         self.assertEqual(len(my_list), expected_items)
 
-    def test_append_works(self):
+
+class LengthTests(unittest.TestCase):
+    @parameterized.expand([(List(["a", "b", "c"]), 3), (List([]), 0), (List(["a"]), 1)])
+    def test_length(self, test_list: List, expected_length: int):
+        # Act
+        result = len(test_list)
+
+        # Assert
+        self.assertEqual(result, expected_length)
+
+    def test_length_push(self):
+        # Arrange
+        test_list = List(["a", "b", "c"])
+        test_list.push("d")
+        expected_length = 4
+
+        # Act
+        result = len(test_list)
+
+        # Assert
+        self.assertEqual(result, expected_length)
+
+    @parameterized.expand(((0,), (1,), (2,)))
+    def test_length_pop(self, index: int):
+        # Arrange
+        test_list = List(["a", "b", "c"])
+        test_list.pop(index)
+        expected_length = 2
+
+        # Act
+        result = len(test_list)
+
+        # Assert
+        self.assertEqual(result, expected_length)
+
+
+class PushTests(unittest.TestCase):
+    def test_push_works(self):
         # Arrange
         my_list: List = List()
         expected = "value"
@@ -40,7 +77,7 @@ class ListItemsTests(unittest.TestCase):
         # Assert
         self.assertEqual(retrieved, expected)
 
-    def test_append_works_on_initialized(self):
+    def test_push_works_on_initialized(self):
         # Arrange
         my_list: List = List(["a", "b", "c"])
         x = "value"
@@ -74,6 +111,12 @@ class IndexTests(unittest.TestCase):
         with self.assertRaises(IndexError):
             self.my_list.get(invalid_index)
 
+
+class PopTests(unittest.TestCase):
+    def setUp(self) -> None:
+        self.my_list: List = List(["a", "b", "c"])
+        return super().setUp()
+
     @parameterized.expand(
         [(0, "['b', 'c']", "a"), (1, "['a', 'c']", "b"), (2, "['a', 'b']", "c")]
     )
@@ -91,15 +134,20 @@ class IndexTests(unittest.TestCase):
         self.assertEqual(result, expected_result)
         self.assertEqual(result_str, expected_str)
 
-    @parameterized.expand([(List([1]), 1, "[]")])
-    def test_remove_by_index_special(
-        self, in_list: List, expected_result: int, expected_str: str
-    ):
-        result = in_list.pop(0)
+    def test_remove_by_index_one_item(self):
+        # Arrange
+        single_item_list = List([1])
+        expected_result: int = 1
+        expected_str: str = "[]"
 
+        # Act
+        result = single_item_list.pop(0)
+
+        # Assert
         self.assertEqual(expected_result, result)
-        self.assertEqual(expected_str, str(in_list))
+        self.assertEqual(expected_str, str(single_item_list))
 
-    def test_remove_by_invalid_index(self):
+    @parameterized.expand([(List(),)])
+    def test_remove_by_invalid_index(self, initial_list: List):
         with self.assertRaises(IndexError):
-            self.my_list.pop(4)
+            initial_list.pop(0)
